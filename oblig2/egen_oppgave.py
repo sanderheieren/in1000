1f56be2d0c3ebdac4aba6888618b1d072ebb0e53
sanderheieren <sanheieren@gmail.com>
2021-01-27 21:43:54 +0100

    ferdig

---
+++ oblig2/egen_oppgave.py
@@ -0,0 +1,55 @@
+# 4 quiz spørsmål om teknologier som brukes ofte i web browseren
+
+def sporsmaal(spm):
+    print('_______________')
+    print(spm)
+    return input()
+
+
+def riktig():
+    print('Bra, det var riktig!')
+
+
+def feil():
+    print('Det var dessverre ikke riktig.')
+
+
+print('Velkommen til Web Quiz 101')
+
+riktige_svar = 0
+antall_spm = 0
+
+if sporsmaal('Hvilket programmeringspråk kjører i web browseren?').lower() == "javascript":
+    riktige_svar += 1
+    riktig()
+else:
+    feil()
+
+if sporsmaal('Hva står det kjente styling-programmet "CSS" for?').lower() == "cascading style sheets":
+    riktige_svar += 1
+    riktig()
+else:
+    feil()
+
+if sporsmaal('Hva står HTML for?').lower() == "hypertext markup language":
+    riktige_svar += 1
+    riktig()
+else:
+    feil()
+
+if sporsmaal('I hvilket år ble JavaScript lansert?') == "1996":
+    riktige_svar += 1
+    riktig()
+else:
+    feil()
+
+print('_______________')
+print(f"Du klarte å oppnå {riktige_svar} poeng!")
+if riktige_svar == 0:
+    print("Øvelse gjør mester")
+elif riktige_svar == 4:
+    print("Alt riktig!")
+elif riktige_svar >= 2:
+    print("Dette var ok, men du kan bedre")
+else:
+    print('Bedre enn gjennomsnittet')
--- oblig2/kodeforstaaelse.py
+++ oblig2/kodeforstaaelse.py
@@ -1,8 +1,10 @@
 # Teorispørsmål:
+
 # 1:
 # Nei, dette er ikke lovlig kode. Enkelt og greit fordi det ikke er mulig å legge sammen
 # en string og en tallverdi. Det er to ulike datatyper, og vi vil få en feilmelding.
 # Koden er forøvrig 'lovlig' frem til print-setningen.
+
 # 2:
 # Vi får en feilmelding på linje 4, gitt at heltallet, b, er mindre enn 10. Ellers vil den
 # aldri komme til print-setningen, og koden vil forsette som normalt.
