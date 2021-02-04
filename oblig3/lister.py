17712ca2ef614645ec6b4861340c4f21bc3bf547
sanderheieren <sanheieren@gmail.com>
2021-02-04 23:00:46 +0100

    oblig3 start

---
+++ oblig3/lister.py
@@ -0,0 +1,9 @@
+# Dette programmet skal inneholde en liste som har styring på tall jeg velger selv
+# samt input fra brukeren. Med disse tallene skal jeg gjøre litt manipulasjon for å
+# vise jeg behersker det nye teamet om "Lister"
+
+# 1
+listeHeltall = [4, 2, 8]
+listeHeltall.append(10)
+print(
+    f"Første element i listen er: {listeHeltall[0]}.\nTredje element i lista er: {listeHeltall[2]}.")
