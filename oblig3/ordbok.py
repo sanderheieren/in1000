e132684629ebbbfc6060e3e91ea6fa74a41faf18
sanderheieren <sanheieren@gmail.com>
2021-02-06 17:13:39 +0100

    opg 2

---
+++ oblig3/ordbok.py
@@ -0,0 +1,30 @@
+# Dette programmet skal lage en ordbok som inneholder ulike varer i en butikk
+# samt den respektive prisen. Varenavnet er nøkklen, mens prisen er innholdet
+# i form av float (flyttall)
+
+# 1, Definerer(?)/Lager en ordbok som inneholder ulike varer + prisen deres.
+
+varer = {
+    "melk": 14.90,
+    "yoghurt": 12.90,
+    "pizza": 39.90
+}
+
+# Printer ut inneholdet
+print(f"Inneholdet i varene er:\n{varer}")
+
+# Definerer prosedyre for å legge til varer i 'varer'-ordboka
+
+
+def leggTilVareNavn():
+    vareNavn = input('Skriv inn navnet på varen: ')
+    varePris = float(input(f'Hvor mye koster {vareNavn}? '))
+    print()
+    varer[vareNavn] = varePris
+
+
+leggTilVareNavn()
+leggTilVareNavn()
+
+# Printer ut resultat etter input fra bruker
+print(varer)
