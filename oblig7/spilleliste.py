a4da1d3afb72ca4fe90effb2a2bd2515d7fdc28f
sanderheieren <sanheieren@gmail.com>
2021-03-17 13:46:27 +0100

    opg2 halv

--- oblig7/spilleliste.py
+++ oblig7/spilleliste.py
@@ -5,3 +5,13 @@ class Spilleliste:
     def __init__(self, listenavn):
         self._sanger = []
         self._navn = listenavn
+
+    def lesFraFil(self, filnavn):
+        fil = open(filnavn)
+
+        for linje in fil:
+            print(linje)
+
+
+spilleliste = Spilleliste('topp 40')
+spilleliste.lesFraFil('musikk.txt')
